e599129565995d2c6df6c790cfbb0967f68eda76
kaiq22unc <sqin@unc.edu>
2022-11-29 01:23:14 -0500

    model

--- src/main/java/com/comp301/a09akari/model/ModelImpl.java
+++ src/main/java/com/comp301/a09akari/model/ModelImpl.java
@@ -22,9 +22,8 @@ public class ModelImpl implements Model {
 
   @Override
   public void addLamp(int r, int c) {
-    if (r >= active.getHeight() || r < 0 || c >= active.getWidth() || c < 0)
-      throw new IndexOutOfBoundsException();
-    //if (active.getCellType(r, c) != CellType.CORRIDOR) throw new IllegalArgumentException();
+    if (r >= active.getHeight() || r < 0 || c >= active.getWidth() || c < 0)  throw new IndexOutOfBoundsException();
+    if (active.getCellType(r,c) != CellType.CORRIDOR) throw new IllegalArgumentException();
     if (!lamb[r][c]) lamb[r][c] = true;
   }
 
@@ -43,22 +42,22 @@ public class ModelImpl implements Model {
     if (active.getCellType(r, c) != CellType.CORRIDOR) throw new IllegalArgumentException();
     int tr = r;
     int tc = c;
-    while (active.getCellType(r, c) == CellType.CORRIDOR && r>=0 && r < active.getHeight()) {
+    while (active.getCellType(r, c) == CellType.CORRIDOR && r>0 && r < active.getHeight()) {
       if (isLamp(r, c)) return true;
       r = r - 1;
     }
     r = tr;
-    while (active.getCellType(r, c) == CellType.CORRIDOR && r>=0 && r < active.getHeight()) {
+    while (active.getCellType(r, c) == CellType.CORRIDOR && r>0 && r < active.getHeight()) {
       if (isLamp(r, c)) return true;
       r = r + 1;
     }
     r = tr;
-    while (active.getCellType(r, c) == CellType.CORRIDOR && c>=0 && c < active.getWidth()) {
+    while (active.getCellType(r, c) == CellType.CORRIDOR && c>0 && c < active.getWidth()) {
       if (isLamp(r, c)) return true;
       c = c - 1;
     }
     c = tc;
-    while (active.getCellType(r, c) == CellType.CORRIDOR && c>=0&& r < active.getWidth()) {
+    while (active.getCellType(r, c) == CellType.CORRIDOR && c>0&& r < active.getWidth()) {
       if (isLamp(r, c)) return true;
       c = c + 1;
     }
